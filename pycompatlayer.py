c5804da8ab8a0a71a75e74bb46cde4fa1890b137
ale5000 <ale5000-git@users.noreply.github.com>
2016-09-17 15:04:20 +0200

    Minor change

--- pycompatlayer.py
+++ pycompatlayer.py
@@ -1,4 +1,4 @@
-"""PyCompatLayer - compatibility layer for Python.
+"""PyCompatLayer - Compatibility layer for Python.
 
 It make all versions of Python behaving as the latest version of Python 3.x.
 This will allow you to be compatible with all versions of Python without effort.
