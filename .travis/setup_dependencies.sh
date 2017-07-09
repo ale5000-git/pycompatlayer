09d7a053c9bceeb1c318b8e848b6b8551f511d6c
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-09 02:04:48 +0200

    Improved testing environment

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -5,13 +5,13 @@ echo '*** Installing dependencies...'
 if [[ $TRAVIS_PYTHON_VERSION == '3.2' ]]; then
   pip install coverage==4.0a5 || exit 1
   pip install requests==2.10.0 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   pip install coverage==3.7.1 || exit 1
 else
   :  # Coverage and requests are automatically installed by codecov.
 fi
 
-if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+if [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
   pip install unittest2==0.5.1 || exit 1
 else
   pip install unittest2 || exit 1
--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '*** Python Virtualenv setup in progress...'
+echo '*** Python - Virtualenv setup in progress...'
 virtualenv "~/virtualenv/python$1" || exit 1
 rm -f "~/virtualenv/python$1/bin/python" || exit 1
 virtualenv -p "/usr/bin/python$1" "~/virtualenv/python$1" || exit 1
@@ -8,15 +8,14 @@ source "~/virtualenv/python$1/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$1"
 
-if [[ $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
-  echo '*** Installing Python Pip...'
-  pip install pip==1.1 || exit 1
-
-  echo '*** Installing Python Setuptools...'
-  pip install setuptools==0.7.3 > /dev/null 2>&1 || exit 1
+if [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+  echo '*** Python - Installing Setuptools...'
+  easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
   rm -rf "~/virtualenv/python$1/lib/python$1/site-packages/distribute-"* || exit 1
+  easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz' || exit 1
 
-  pip install setuptools==1.4.2 || exit 1
+  echo '*** Python - Installing Pip...'
+  pip install pip==1.1 || exit 1
 fi
 
 echo '*** Done.'
