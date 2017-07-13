67cf80b70cb216d911f2f6ba0f8537ad68682b60
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-13 02:35:51 +0200

    Improved testing environment

--- .travis/setup_virtualenv.sh
+++ .travis/setup_virtualenv.sh
@@ -3,17 +3,17 @@
 if [[ -n "$1" ]]; then VER="$1"; else VER="$Python"; fi
 
 echo '*** Python - Virtualenv setup in progress...'
-virtualenv -p "python$VER" --setuptools "~/virtualenv/python$VER" || exit 1
-source "~/virtualenv/python$VER/bin/activate" || exit 1
+virtualenv -p "python$VER" --setuptools "$HOME/virtualenv/python$VER" || exit 1
+source "$HOME/virtualenv/python$VER/bin/activate" || exit 1
 
 export TRAVIS_PYTHON_VERSION="$VER"
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
+if [[ $VER == '3.1' ]]; then
   echo '*** Python - Updating Setuptools...'
   easy_install 'https://pypi.python.org/packages/source/s/setuptools/setuptools-0.7.3.tar.gz' > /dev/null 2>&1 || exit 1
-  rm -rf "~/virtualenv/python$VER/lib/python$VER/site-packages/distribute-"* || exit 1
+  rm -rf "$HOME/virtualenv/python$VER/lib/python$VER/site-packages/distribute-"* || exit 1
   pip install setuptools==19.4 || exit 1
-elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
+elif [[ $VER == '2.5' || $VER == '2.4' ]]; then
   echo '*** Python - Updating Setuptools...'
   pip install setuptools==1.4.2 || exit 1
 
