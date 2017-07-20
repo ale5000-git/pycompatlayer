6c141ba5a257422be04d59ec944d258fc0aaf341
ale5000 <ale5000-git@users.noreply.github.com>
2017-07-20 19:15:41 +0200

    Updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -12,11 +12,12 @@ author = 'ale5000'
 # The master toctree document.
 master_doc = 'index'
 
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'pycompatlayer')))
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest'
 ]
-
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'pycompatlayer')))
--- docs/index.rst
+++ docs/index.rst
@@ -6,7 +6,8 @@ Contents:
 .. toctree::
    :maxdepth: 2
 
-   main
+   intro
+   api
 
 
 Indices and tables
