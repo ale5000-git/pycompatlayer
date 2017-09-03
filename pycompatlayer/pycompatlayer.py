ec35a263af957befa987b16b9c97aa81f030ea8e
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-03 17:59:58 +0200

    Add BaseException if missing

--- pycompatlayer/pycompatlayer.py
+++ pycompatlayer/pycompatlayer.py
@@ -126,6 +126,10 @@ def fix_builtins(override_debug=False):
     if builtins_dict.get(__name__, False):
         raise RuntimeError(__name__+" already loaded")
 
+    # Exceptions
+    if builtins_dict.get("BaseException") is None:
+        override_dict["BaseException"] = Exception
+
     if 'format' not in str.__dict__:
         override_dict["str"] = _Internal.ExtStr
     # Function 'input'
