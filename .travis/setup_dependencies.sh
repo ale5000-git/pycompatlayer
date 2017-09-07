d24e389d7e2cf56acb8059ccb2934019f6012b9f
ale5000 <ale5000-git@users.noreply.github.com>
2017-09-08 01:47:49 +0200

    Switch to trusty

--- .travis/setup_dependencies.sh
+++ .travis/setup_dependencies.sh
@@ -10,7 +10,7 @@ else
   pip install unittest2 || exit 1
 fi
 
-if [[ $TRAVIS_PYTHON_VERSION == '3.2' || $TRAVIS_PYTHON_VERSION == '3.1' || $TRAVIS_PYTHON_VERSION == 'pypy3-2.4.0' ]]; then
+if [[ $TRAVIS_PYTHON_VERSION == '3.2' || $TRAVIS_PYTHON_VERSION == '3.1' ]]; then
   pip install coverage==4.0a5 || exit 1
   pip install requests==2.10.0 || exit 1
 elif [[ $TRAVIS_PYTHON_VERSION == '2.5' || $TRAVIS_PYTHON_VERSION == '2.4' ]]; then
