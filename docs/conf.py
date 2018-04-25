594bc0a4d109281afa9aa8ecf7a232cb09b483e9
ale5000 <ale5000-git@users.noreply.github.com>
2018-04-25 03:34:28 +0200

    The hack to fix the extensions of internal links is now more generic

--- docs/conf.py
+++ docs/conf.py
@@ -7,15 +7,32 @@ import datetime
 import fileinput
 import re
 
+
+def replace_ext_inside_documentation_files(path, filename):
+    for line in fileinput.input(path+filename, inplace=1, backup=".bak", mode="rb"):
+        # Hack to fix the extension of internal links in the documentation
+        sys.stdout.write(re.sub(r"\`(.+)\s<([\w\.]+)\.rst>\`_", r"`\1 <\2.html>`_", line.decode("utf-8")))
+    sys.stdout.flush()
+
+
+def find_and_fix_documentation_files(path):
+    dir_list = tuple(sorted(os.listdir(path)))
+    if len(dir_list) != 0:
+        for filename in dir_list:
+            if filename.endswith(".rst"):
+                sys.stdout.write("Fixing "+path+filename+" ..." + os.linesep)
+                replace_ext_inside_documentation_files(path, filename)
+    sys.stdout.write("Done." + os.linesep + os.linesep)
+    sys.stdout.flush()
+
+
 # General information about the project.
 project = 'PyCompatLayer'
 copyright = '2016-' + str(datetime.datetime.now().year) + ', ale5000'
 author = 'ale5000'
 
-# Hack to fix the extension of internal links in the generated documentation
-for line in fileinput.input("../README.rst", inplace=1, backup=".bak", mode="rb"):
-    sys.stdout.write(re.sub(r"\`(.+)\s<([\w\.]+)\.rst>\`_", r"`\1 <\2.html>`_", line.decode("utf-8").rstrip()) + "\n")
-sys.stdout.flush()
+find_and_fix_documentation_files("../")
+find_and_fix_documentation_files("./")
 
 # The master toctree document.
 master_doc = 'index'
